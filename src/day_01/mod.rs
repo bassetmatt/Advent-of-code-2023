043622b69627cde40876e0a7af13fbc430918eaf
matt <bassetmatt@gmail.com>
2023-12-01 00:34:32 -0500

    Day 1 - Part 1

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,5 +1,20 @@
 use crate::read_file;
 
 pub fn run() {
-	let _input = read_file("src/day_01/input");
+	let input = read_file("src/day_01/input");
+	let mut sum = 0;
+	for line in input.split('\n') {
+		let mut first_num = None;
+		let mut last_num = None;
+		for c in line.chars() {
+			if c.is_numeric() {
+				if first_num.is_none() {
+					first_num = Some(c as u32 - '0' as u32);
+				}
+				last_num = Some(c as u32 - '0' as u32);
+			}
+		}
+		sum += first_num.unwrap_or(0) * 10 + last_num.unwrap_or(0);
+	}
+	println!("Sum: {}", sum);
 }
